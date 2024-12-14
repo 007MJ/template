f4ddd7d812cfa82085622012d9429c7cc71552f6
007MJ <k.mijiza@gmail.com>
2024-12-14 12:50:17 -0500

    rest ajouter le lombre

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 
-class UserCard extends HTMLElement  {
-    constructor () {
-        super();
-        this.attachShadow ({ mode: 'open'});
-        this.shadowRoot.appendChild(template.conent.cloneNOde)
-        this.innerHTML = `
-        <h1> ${this.getAttribute('name')}</h1>
-        `;
-    }
-}
+const element = document.getElementById("regis-form");
+console.log(element);
+element.addEventListener("click", function() {
+    console.log("Bonjour ---------");
+    document.getElementById("login-page").remove();
+});
 
-
-window.customElements.define('user-card', UserCard);
+// console.log("gang !")
